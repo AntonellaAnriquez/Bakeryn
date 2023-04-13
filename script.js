521d80ff5338920d0daaa19b90b3a8b7838b3df8
AleexPoch <123516772+AleexPoch@users.noreply.github.com>
2023-04-13 20:52:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,17 @@ async function getProducts(){
     
 }
 getProducts();
+
+function scrolltoTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
