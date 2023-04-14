278793f5cf99016f386c3e234344ba08bfa0b1d9
AntonellaAnriquez <anriquezan@gmail.com>
2023-04-14 00:00:15 -0300

    Carousel / minor fixes css

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 async function fetchProductos() {
-    try{
-        const response = await fetch("./productos.json");
-        const data = await response.json();
-        return data.productos;
-    }catch(error){
-        console.log(error);
-    }
+  try {
+    const response = await fetch("./productos.json");
+    const data = await response.json();
+    return data.productos;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const contenedor = document.querySelector("#contenedorProducto");
 
-async function getProducts(){
-    const productos = await fetchProductos();
-    productos.forEach((producto) => {
-        contenedor.innerHTML += 
-        `
+async function getProducts() {
+  const productos = await fetchProductos();
+  productos.forEach((producto) => {
+    contenedor.innerHTML += `
         <div class="tarjeta">
-            <img src=${producto.img} alt=${producto.nombre}>
+        <img src=${producto.img} alt=${producto.nombre}>
             <h3>${producto.nombre}</h3>
             <p>$${producto.precio}</p>
             <p class="descripcion">${producto.descripcion}</p>            
             <button class="btn">COMPRAR AHORA</button>
-        </div> 
-        `
-      })
-  
-    
+            </div> 
+        `;
+  });
 }
 getProducts();
 
